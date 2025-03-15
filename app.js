33024a722fe070309d25b7cc08e73c16ea920c7c
gretakay <monica24685@gmail.com>
2025-03-15 16:46:09 +0800

    name

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/diligent-pwa/service-worker.js')
         .then(registration => {
             console.log('Service Worker 註冊成功:', registration);
         })
